18cc113de352f55501b7722e777e173e8e736818
elmira <elledevelope@gmail.com>
2024-10-14 14:47:12 +0200

    Close any open submenus

--- assets/script.js
+++ assets/script.js
@@ -14,45 +14,39 @@ const sousMenu = [  // <li> for the sub-menu (child-sub)
 ];
 
 //----------------------------------------------------FONCTIONS :
-let switchMenu1 = false; //<--------------- initial state
-
 function openMenu(liIdNumber) {
-    // console.log(liIdNumber);
-
     //Creating sub-<ul> on click on main-<li> :
-    document.getElementById(`item${liIdNumber}`).addEventListener("click", function () {
-        // console.log("Hello");
+    document.querySelector("#item" + liIdNumber).addEventListener("click", function () {
+        console.log("Hello");
 
-        // closing sub-menus :
+        // Close any open submenus
         for (let i = 0; i < sousMenu.length; i++) {
-            if (document.querySelector(`#item${liIdNumber} ul`) != undefined) {
-                document.querySelector(`#item${liIdNumber} ul`).remove();
-            }
-        };
+            // if (document.querySelector(#item${i} ul) != undefined) {
+            //     document.querySelector(#item${i} ul).remove();
+            // };
+            const openSubMenu = document.querySelector(`#item${i} ul`);
+            if (openSubMenu) openSubMenu.remove(); // Only remove if the submenu exists
+        }
 
-        if (!switchMenu1) { // Evaluates to true if switchMenu1 is false
-            console.log(switchMenu1);
+
+        // Check if the submenu for this item already exists
+        let existingSubMenu = document.querySelector(`#item${liIdNumber} ul`);
+        if (!existingSubMenu) {  // If it doesn't exist, create a new one
+        // if (document.querySelector(#item${liIdNumber} ul) != undefined)
+            console.log("HelloHello");
             const ulSubMenu = document.createElement("ul");  // create sub-<ul>
-            ulSubMenu.className = "ulSubMenu"; // Set the class name for styling
-            document.querySelector(`#item${liIdNumber}`).append(ulSubMenu); // Insert sub-<ul> at the end of <li>
+            ulSubMenu.className = "ulSubMenu";
+            document.querySelector(`#item${liIdNumber}`).append(ulSubMenu); // Append sub-<ul> to <li>
 
-            // Creating sub-<li>
+            // Create and append sub-<li> elements 
             sousMenu[liIdNumber].forEach(function (element) {
                 const liSubMenu = document.createElement("li");
                 liSubMenu.innerText = element; // Insert elements from arraySousMenu
                 ulSubMenu.append(liSubMenu); // Append each sub-item to the sub-menu
             });
-        } else {
-            // Log the current sub-<ul> for debugging
-            console.dir(document.querySelector(`#item${liIdNumber} ul`)); // Search for the existing sub-<ul>
-            document.querySelector(`#item${liIdNumber} ul`).remove(); // Delete sub-<ul> -> closing sub-menu
         }
-
-        // Toggle switchMenu1 for the next click
-        switchMenu1 = !switchMenu1;
-        console.log(switchMenu1);
     });
-};
+}
 
 //----------------------------------------------------LOGIQUE de FONCTION :
 // Loop through the main menu items and add them as <li> elements inside the main <ul>
