fca55e155e0c6ebc5a0040dc303150c571f25adc
elmira <elledevelope@gmail.com>
2024-10-14 12:03:14 +0200

    Update sub-menu

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,14 @@
 // Select the <ul> element inside the #nav element
 const navUl = document.querySelector("#nav>ul");
 
-// List of items for the main menu (parent)
+// <li> for the main menu (parent-main)
 const menuPrincipal = [
     "MainItem1",
     "MainItem2",
     "MainItem3"
 ];
 
-// List of items for the sub-menu (child)
+//  <li> for the sub-menu (child-sub)
 const sousMenu1 = [
     "1--SubItem",
     "2--SubItem",
@@ -22,19 +22,30 @@ while (i < menuPrincipal.length) {
     i++;
 }
 
-//Creating first sub-UL on click on fist LI-parent
+//Creating sub-<ul> on click on fist main-<li> ("MainItem1")
+let switchMenu1 = false; //<--------------- initial state
 document.getElementById("item0").addEventListener("click", function () {
     console.log("Hello");
-    const ulSubMenu = document.createElement("ul");  // Create a new <ul> element for the sub-menu
-    ulSubMenu.className = "ulSubMenu";
-    document.querySelector("#item0").append(ulSubMenu); // append() to insert the new <ul> at the end of the item0 element's content
+    if (!switchMenu1) { // <--------------- (!switchMenu1 = !false  = true) evaluates to true, so the submenu is created:
+        console.log(switchMenu1);
+        const ulSubMenu = document.createElement("ul");  // create sub-<ul> 
+        ulSubMenu.className = "ulSubMenu";
+        document.querySelector("#item0").append(ulSubMenu); // insert sub-<ul> at the end of <li class = "item0"> content
 
-    // Creating sub-LI 
-    sousMenu1.forEach(function (element, index) {
-        const liSubMenu = document.createElement("li");
-        ulSubMenu.append(liSubMenu);
-        liSubMenu.innerHTML = sousMenu1[index];
-    });
+        // Creating sub-<li> 
+        sousMenu1.forEach(function (element, index) {
+            // console.log(element, index);
+            const liSubMenu = document.createElement("li");
+            liSubMenu.innerText = element; // insert sousMenu1 = ["1--SubItem", "2--SubItem", "3--SubItem"];
+            ulSubMenu.append(liSubMenu);
+        });
+
+    }
+    else {
+        console.dir(document.querySelector("#item0 ul")); // search sub-<ul> in main-<li> id = item0
+        document.querySelector("#item0 ul").remove(); // delete sub-<ul> ---------> closing sub-menu
+    };
+    switchMenu1 = !switchMenu1;
+    console.log(switchMenu1);
 });
-
 
