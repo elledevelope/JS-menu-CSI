36552ce2b3c8e92382665bf80ca0e23ffed6ddbd
elmira <elledevelope@gmail.com>
2024-10-14 11:14:28 +0200

    Create sub UL and sub LI

--- assets/script.js
+++ assets/script.js
@@ -3,30 +3,38 @@ const navUl = document.querySelector("#nav>ul");
 
 // List of items for the main menu (parent)
 const menuPrincipal = [
-    "item1",
-    "item2",
-    "item3",
-    "item4",
-    "item5"
+    "MainItem1",
+    "MainItem2",
+    "MainItem3"
 ];
 
 // List of items for the sub-menu (child)
 const sousMenu1 = [
-    "sub-item1",
-    "sub-item2",
-    "sub-item3"
+    "1--SubItem",
+    "2--SubItem",
+    "3--SubItem"
 ];
 
 // Loop through the main menu items and add them as <li> elements inside the main <ul>
-let i = 0; 
+let i = 0;
 while (i < menuPrincipal.length) {
-    navUl.innerHTML += `<li id='item${i}'>${menuPrincipal[i]}</li>`; // Dynamically add <li> with item and ID
-    i++; 
+    navUl.innerHTML += `<li id='item${i}'>${menuPrincipal[i]}</li>`;  // creating <li> with id = item0, item1, etc.
+    i++;
 }
 
-// Add an event listener to the first menu item (item0)
+//Creating first sub-UL on click on fist LI-parent
 document.getElementById("item0").addEventListener("click", function () {
-    console.log("Hello"); 
+    console.log("Hello");
     const ulSubMenu = document.createElement("ul");  // Create a new <ul> element for the sub-menu
-    document.querySelector("#item0").prepend(ulSubMenu); // Insert the new <ul> as the first child (before other content) of item0
+    ulSubMenu.className = "ulSubMenu";
+    document.querySelector("#item0").append(ulSubMenu); // append() to insert the new <ul> at the end of the item0 element's content
+
+    // Creating sub-LI 
+    sousMenu1.forEach(function (element, index) {
+        const liSubMenu = document.createElement("li");
+        ulSubMenu.append(liSubMenu);
+        liSubMenu.innerHTML = sousMenu1[index];
+    });
 });
+
+
