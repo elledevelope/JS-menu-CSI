13aa947900072d1d26ffcb9b888b4a8d476b89b8
elmira <elledevelope@gmail.com>
2024-10-17 11:02:55 +0200

    Globalized const menu and menuData

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 import { menuData } from "./modules/menuData.js";
-console.dir(menuData);
-const menu = document.querySelector("#header-nav>ul"); // <ul> inside #nav 
+console.dir(menuData[0].sousMenu[0]);
+
+import {displaySubMenu} from "./modules/displaySubMenu.js";
+console.dir(displaySubMenu);
+
+globalThis.menu = document.querySelector("#header-nav>ul"); // <ul> inside #nav 
+globalThis.menuData = menuData;
+
+displaySubMenu(0);
