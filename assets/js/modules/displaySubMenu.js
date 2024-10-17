51c85a253a4e25a5221a66e8d5c590e68992a718
elmira <elledevelope@gmail.com>
2024-10-17 14:11:31 +0200

    Update displayElement parameter - parent

--- assets/js/modules/displaySubMenu.js
+++ assets/js/modules/displaySubMenu.js
@@ -2,8 +2,8 @@ import { liCreate } from "./liCreate.js";
 import { menuData } from "./menuData.js";
 import { ulCreate } from "./ulCreate.js";
 
-const displaySubMenu = (index, classArray = undefined) => {
-    const ulSubMenu = ulCreate(menu, "idUlSubMenu" + index, classArray); // const ulCreate = (parent, idName, classArray = [])
+const displaySubMenu = (parent, index, classArray = undefined) => {
+    const ulSubMenu = ulCreate(parent, "idUlSubMenu" + index, classArray); // sub <ul> => const ulCreate = (parent, idName, classArray = [])
 
     menuData[index].sousMenu.forEach((element, index2) => {
         liCreate(ulSubMenu, element);  // sub <li>
