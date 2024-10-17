41b744f2189a5f7b07699d473ac9994239e5592d
elmira <elledevelope@gmail.com>
2024-10-17 10:01:45 +0200

    Add ID facultatif and classList in liCreate.js

--- assets/js/modules/liCreate.js
+++ assets/js/modules/liCreate.js
@@ -1,7 +1,16 @@
-const liCreate = (parent, txt) => {
+const liCreate = (parent, txt, idName = undefined, classArray = []) => { // liCreate(document, "text", undefined, ["className"]);
     const li = document.createElement("li");
     li.innerText = txt;
+    li.classList.add(classArray);
+
+    // id facultatif :
+    if (idName != undefined) {
+        li.id = idName;
+    };
+
     parent.append(li);
 };
+
+
 
 export { liCreate };
